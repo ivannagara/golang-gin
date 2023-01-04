53cca10ec69e8cd5a800e08becc62beb5b27d918
ivan nagara <ivannagara@gmail.com>
2023-01-04 16:38:02 +0800

    delete recipe endpoint

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
 
@@ -34,7 +35,7 @@ func NewRecipeHandler(c *gin.Context) {
 	recipe.ID = xid.New().String()
 	recipe.PublishedAt = time.Now()
 	recipes = append(recipes, recipe)
-	c.JSON(http.StatusOK, recipe)
+	c.JSON(http.StatusOK, recipes)
 }
 
 func ListRecipesHandler(c *gin.Context) {
@@ -62,6 +63,24 @@ func UpdateRecipeHandler(c *gin.Context) {
 	}
 	recipes[index] = recipe
 	c.JSON(http.StatusOK, recipe)
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted"})
 }
 
 type Recipe struct {
